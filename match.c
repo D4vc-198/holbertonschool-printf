744aa4ac0e62af24b50b86855ee796bd6e196bb1
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-09 13:57:21 -0600

    Se agrego el archivo: match.c

---
+++ match.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * match_case - function that matches the flag passed as an argument
+ * @flag: pointer to flag
+ * Return: pointer to function associated to flag
+ */
+
+int (*match_case(const char *flag))(va_list)
+{
+	int pos = 0;
+
+	c_mtc flag_store[] = {
+		{"s", string_case},
+		{"c", char_case},
+	}
+}
