8b1c6c4df510afd0dec5e278d4e5b37bbf06c517
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-09 13:14:12 -0600

    Se agrego el archivo: main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdgarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+/**
+ * struc case_match - stores a flag and function
+ * @c: pointer to the flag
+ * @f: pointer to function
+ */
+
+typedef struct case_match
+{
+
+}
