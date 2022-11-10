c550e7a833dd2744c8897e3c25475d49523a0f5e
Javi Icaza <javiicaza@gmail.com>
2022-11-09 22:18:15 -0600

    main.h

--- main.h
+++ main.h
@@ -11,9 +11,27 @@ int _printf(const char *format, ...);
  * struc case_match - stores a flag and function
  * @c: pointer to the flag
  * @f: pointer to function
+ *
+ *Description: Longer description
  */
-
 typedef struct case_match
 {
+	char *c;
+	int (*f)(va_list);
+} c_mtc;
+/* ------------------------------------*/
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *);
+char *_strcpy(char *, char *);
+char *_strcat(char *, char *);
+/* --------------------------- */
+int char_case(va_list);
+int string_case(va_list);
+int digit_base_10(va_list);
+/*------------------------------------*/
+int restriction_percentage(const char *str);
+int (*match_case(const char *))(va_list);
+int convert_base(int base, int number, int band);
 
-}
+#endif
