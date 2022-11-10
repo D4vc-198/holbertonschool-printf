229471ee8146d57b8a547232da864af2899fae8f
Javi Icaza <javiicaza@gmail.com>
2022-11-09 22:27:43 -0600

    added functions and betty review

--- printer.c
+++ printer.c
@@ -44,4 +44,48 @@ int _strlen(char *s)
 
 /**
  * _strcpy - copy string pointed to src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
  *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n, i;
+
+	n = 0;
+	while (src[n] != 0)
+	{
+		n++;
+	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x = 0;
+	int y = 0;
+
+	while (dest[x])
+	{
+		x++;
+	}
+	while (src[y])
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	return (dest);
+}
