4fa453da11427a3123bd469750071614354d8467
d4vc-198 <contactodiegoavc@gmail.com>
2022-11-11 15:25:50 -0600

    se actualizo functions.c y printer.c

--- functions.c
+++ functions.c
@@ -5,6 +5,7 @@
  * @base: number of base to convert
  * @number: Number to convert
  * @band: flag
+ * @add: A pointer pointing to a memory address within the buffer
  * Return: lenght of string
  */
 
@@ -25,8 +26,7 @@ int convert_base(int base, int number, int band, char **add)
 
 	if (number == 0)
 	{
-		**add = 48;
-		(**add)++;
+		**add = 48, (*add)++;
 		return (1);
 	}
 	/*Convert*/
@@ -46,9 +46,8 @@ int convert_base(int base, int number, int band, char **add)
 
 	if (_strlen(ptr) < 2 && band == 2)
 	{
-		**add = 0;
-		(*add)++;
-		len_end++;
+		**add = 0, (*add)++;
+		len_end++; /*_putchar('0');*/
 	}
 	len_end += _strlen(ptr);
 	return (write(1, ptr, len_end));
@@ -57,7 +56,7 @@ int convert_base(int base, int number, int band, char **add)
 /**
  * print_number - prints an integer.
  * @n: integer n to print using _putchar
- *
+ * @add: A pointer pointing to a memory address within the buffer
  * Return: none - void function
  */
 int print_number(long int n, char **add)
