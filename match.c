ee329c87050dae8651c8b9291181c33f7f6c3f01
Javi Icaza <javiicaza@gmail.com>
2022-11-10 22:00:30 -0600

    Flags and functions added

--- match.c
+++ match.c
@@ -15,14 +15,19 @@ int (*match_case(const char *flag))(va_list)
 		{"c", char_case},
 		{"d", digit_base_10},
 		{"i", digit_base_10},
+		{"b", digit_base_2},
+		{"x", digit_base_16_lower},
+		{"X", digit_base_16_upper},
+		{"o", digit_base_8},
+		{"u", digit_case_u},
 		{NULL, NULL}
 	};
 
-	while (pos < 4)
+	while (pos < 9)
 	{
-		if (*flag == *(flag_store[pos]).c)
-			return (*(flag_store[pos]).f);
+		if (*(flag_store[pos]).c == *flag)
+			return (flag_store[pos].f);
 		pos++;
 	}
-	return (*(flag_store[pos]).f);
+	return (flag_store[pos].f);
 }
