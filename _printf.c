680790c9019ed3a5578fed3ccd6e3453567fcec0
Javi Icaza <javiicaza@gmail.com>
2022-11-11 22:54:06 -0600

    Changes for GCC

--- _printf.c
+++ _printf.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* _printf - Prints the string
-* @format: A variable that points to a list of arguments
-* @...: The rest of the arguments
-* Return: the lenght of the printed string
-*/
+ * _printf - Prints the string
+ * @format: A variable that points to a list of arguments
+ * @...: The rest of the arguments
+ *
+ * Return: the length of the printed string
+ */
 int _printf(const char *format, ...)
 {
 	int pos;
@@ -18,32 +19,33 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (*(format) == '\0')
 		return (0);
-
 	for (pos = 0; *(format + pos) != '\0'; pos++)
 	{
 		if (*(format + pos) == '%' && *(format + pos + 1) == '%')
 		{
 			*add = *(format + pos);
 			add++;
+			/*count +=write(1, format + pos, 1);*/
 			pos++;
 			continue;
-
-			if (*(format + pos) == '%' && *(format + pos + 1) == '\0')
-			{
-				return (-1);
-			}
-			if (*(format + pos) == '%')
-			{
-				if (match_case(format + pos + 1) != NULL)
-				{
-					match_case(format + pos + 1)(ptr, &add);
-					pos++;
-					continue;
-				}
-			}
-			*add = *(format + pos);
-			add++;
 		}
-		va_end(ptr);
-		return (_putchar(buff, add - (char *)buff));
+		if (*(format + pos) == '%' && *(format + pos + 1) == '\0')
+		{
+			return (-1);
+		}
+		if (*(format + pos) == '%')
+		{
+			if (match_case(format + pos + 1) != NULL)
+			{
+				match_case(format + pos + 1)(ptr, &add);
+				pos++;
+				continue;
+			}
+		}
+		*add = *(format + pos);
+		add++;
+		/*count += write(1, format + pos, 1);*/
+	}
+	va_end(ptr);
+	return (_putchar(buff, add - (char *)buff));
 }
